4cf3a30d162b83b0aeabf43ee8ef2f71bfaaaa5e
Yaniv Kabariti <kabariti2@gmail.com>
2022-11-20 10:05:20 +0200

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
+import { notFound, errorHandler } from './middlewares';
+import routes from './routes';
+import MessageResponse from './interfaces/MessageResponse';
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get<{}, MessageResponse>('/', (req, res) => {
+  res.json({
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+  });
+});
+
+app.use('/api/v1', routes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
