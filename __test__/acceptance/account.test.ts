6388897bdfffb813809b076f5873a3098e0c6904
Yaniv Kabariti <kabariti2@gmail.com>
2022-11-20 16:05:33 +0200

    updated tests

--- __test__/acceptance/account.test.ts
+++ __test__/acceptance/account.test.ts
@@ -3,7 +3,7 @@ import app from '../../src/app';
 import { Account } from '../../src/models/account';
 import { Persons } from '../../src/models/person';
 
-describe('POST /api/v1/accounts', () => {
+describe('Success POST /api/v1/accounts', () => {
   it('should return the created account for the first user', async () => {
     Persons.find({}).then((persons) => {
       console.log(persons[0]);
@@ -16,6 +16,13 @@ describe('POST /api/v1/accounts', () => {
         expect(response.body).toHaveProperty('_id');
         expect(response.body).toHaveProperty('personId');
       });
+    });
+  });
+});
+describe('Failure POST /api/v1/accounts', () => {
+  it('should failed create account', async () => {
+    request(app).post('/api/v1/accounts').set('Accept', 'application/json').send({ balance: 1000 }).then((response) => {
+      expect(response.statusCode).toEqual(400);
     });
   });
 });
